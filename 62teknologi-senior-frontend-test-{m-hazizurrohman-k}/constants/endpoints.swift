081854070dc14961ae49df7c35366cd9f631ad2e
mhazizk <mhazizk@gmail.com>
2023-04-20 00:14:28 +0700

    Create endpoints.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/constants/endpoints.swift
@@ -0,0 +1,14 @@
+//
+//  endpoints.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 19/04/23.
+//
+
+import Foundation
+
+
+let endpoints = [
+    "searchBusiness" : "https://api.yelp.com/v3/businesses/search",
+    "getBusinessById" : "https://api.yelp.com/v3/businesses/"
+]
