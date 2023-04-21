c934f18b472469b54bed639de5e7a44c318be0a3
mhazizk <mhazizk@gmail.com>
2023-04-22 00:33:52 +0700

    Update getBusinessById.swift

--- 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/networking/getBusinessById.swift
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/networking/getBusinessById.swift
@@ -2,30 +2,36 @@
 //  getBusinessById.swift
 //  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
 //
-//  Created by Haziz on 19/04/23.
+//  Created by Haziz on 21/04/23.
 //
 
 import Foundation
+import SwiftUI
 
 
-extension URLSession {
-    /**
-            `getBusinessById` will asynchronously call Yelp endpoint and return business details from the given `id`
-     */
-    func getBusinessById(at request: URLRequest, completion: @escaping (Result<BusinessDetailsType, Error>) -> Void) {
-    self.dataTask(with: request) { (data, response, error) in
-      if let error = error {
-        completion(.failure(error))
-      }
-
-      if let data = data {
-        do {
-            let business = try JSONDecoder().decode(BusinessDetailsType.self, from: data)
-          completion(.success(business))
-        } catch let decoderError {
-          completion(.failure(decoderError))
-        }
-      }
-    }.resume()
-  }
-}
+func getBusinessById(
+    isLoading: Binding<Bool>,
+//    isFetched:Binding<Bool>,
+    businessId: String,
+    data: Binding<BusinessDetailsType?>) -> Void {
+        
+        isLoading.wrappedValue = true
+//        isFetched.wrappedValue = false
+        
+        let req = request(BaseURLOptions.getBusinessById, params: [], businessId: businessId)
+        
+        URLSession.shared.getBusinessById(at: req, completion: {
+            result in
+            switch result {
+            case .success(let fetched):
+                data.wrappedValue = fetched
+            case .failure(let error):
+                print(error)
+            }
+        })
+//        isFetched.wrappedValue = true
+        isLoading.wrappedValue = false
+        
+        return
+        
+    }
