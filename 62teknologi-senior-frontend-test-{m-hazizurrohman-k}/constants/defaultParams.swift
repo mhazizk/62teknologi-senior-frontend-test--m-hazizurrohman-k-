1c7ed87da043baa1c8b2e7b5286d96c1ca93312b
mhazizk <mhazizk@gmail.com>
2023-04-22 00:34:33 +0700

    Create defaultParams.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/constants/defaultParams.swift
@@ -0,0 +1,11 @@
+//
+//  defaultParams.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 21/04/23.
+//
+
+import Foundation
+
+
+let defaultParams : [String] = ["location=singapore","term=","sort_by=best_match", "offset=0", "limit=20"]
