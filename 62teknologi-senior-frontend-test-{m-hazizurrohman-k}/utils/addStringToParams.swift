aae006a5b374c5c24ee72203f1cdca0073795e65
mhazizk <mhazizk@gmail.com>
2023-04-22 00:34:29 +0700

    Create addStringToParams.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/utils/addStringToParams.swift
@@ -0,0 +1,19 @@
+//
+//  addStringToParams.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 21/04/23.
+//
+
+import Foundation
+
+func addStringToParams(params:[String], stringToAdd:[String], avoidDuplicate:Bool = false) -> [String] {
+    
+    var existingParams = params
+    
+    if avoidDuplicate {
+        existingParams =  removeStringFromParams(params: params, stringToRemove: stringToAdd)
+    }
+    let newParams = existingParams + stringToAdd
+    return newParams
+}
