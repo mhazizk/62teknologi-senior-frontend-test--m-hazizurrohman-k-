71361f8e73b347276b9062164e8551e20d022a05
mhazizk <mhazizk@gmail.com>
2023-04-22 00:33:59 +0700

    Create removeStringFromParams.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/utils/removeStringFromParams.swift
@@ -0,0 +1,19 @@
+//
+//  removeStringFromParams.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 21/04/23.
+//
+
+import Foundation
+
+
+func removeStringFromParams(params:[String], stringToRemove:[String]) -> [String] {
+    var newParams = params
+    if stringToRemove.count == 1 {
+        newParams = params.filter({!$0.contains(stringToRemove[0])})
+    } else {
+        newParams = params.filter({ !stringToRemove.contains($0)})
+    }
+    return newParams
+}
