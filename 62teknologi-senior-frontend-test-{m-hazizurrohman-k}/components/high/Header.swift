f427a8c84652c69cb92cc063e0b171d04b134ee9
mhazizk <mhazizk@gmail.com>
2023-04-22 00:34:49 +0700

    Create Header.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/components/high/Header.swift
@@ -0,0 +1,33 @@
+//
+//  Header.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 21/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+func Header(business:BusinessDetailsType) -> some View {
+    return GeometryReader {
+        geometry in
+        ZStack (alignment: .top) {
+            ImageCarousel(imageUrl: business.photos)
+            HStack {
+                VStack(alignment: .leading) {
+                    
+                    Spacer()
+                    Text(business.name).bold().foregroundColor(.white).font(.largeTitle)
+                    HStack (spacing: 4){
+                        getStarImage(rating: business.rating)
+                        Text(String(business.review_count)).bold().foregroundColor(.white)
+                    }
+                }
+                Spacer()
+            }
+            .padding(16)
+    
+        }
+//        .edgesIgnoringSafeArea(.top)
+    }
+}
