52e47fb35ff407f6cfa97871a05f9b69270a8c84
mhazizk <mhazizk@gmail.com>
2023-04-22 22:39:25 +0700

    Create InfoDetailsSection.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/components/high/InfoDetailsSection.swift
@@ -0,0 +1,52 @@
+//
+//  Section.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 22/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+func InfoDetailsSection(business:BusinessDetailsType) -> some View {
+    
+    func onPress() -> Void {
+        return
+    }
+    
+    return VStack {
+        HStack {
+            Text("Info")
+                .bold()
+                Spacer()
+        }
+        HStack{
+            VStack {
+                HStack {
+                    Text("Hours")
+                        .bold()
+                        Spacer()
+                }
+                HStack(spacing: 4) {
+                    if business.is_closed {
+                        Pill(name: "Closed", color: .red)
+                    } else {
+                        Pill(name: "Open", color: .green)
+                    }
+                    Spacer()
+                }
+            }
+            Spacer()
+            IconButton(iconName: .globe, onPress: onPress)
+            
+        }
+        
+        Color.gray.opacity(0.2).frame(height: 1).padding(.vertical,16)
+        Text("Website")
+            .bold()
+            .font(.subheadline)
+        Text(business.url!)
+        
+    }
+    .padding(16)
+}
