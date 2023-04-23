eb6c0097d419193f3a2838da47f21bef3823886f
mhazizk <mhazizk@gmail.com>
2023-04-23 23:37:44 +0700

    Create getBusinessReviewsById.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/networking/getBusinessReviewsById.swift
@@ -0,0 +1,33 @@
+//
+//  getBusinessReviewsById.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 23/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+func getBusinessReviewsById(
+    isLoading: Binding<Bool>,
+    businessId: String,
+    data: Binding<BusinessReviewsType?>) -> Void {
+        
+        isLoading.wrappedValue = true
+        
+        let req = request(BaseURLOptions.getBusinessReviewsById, params: [], businessId: businessId)
+        
+        URLSession.shared.getBusinessReviewsById(request: req, completion: {
+            result in
+            switch result {
+            case .success(let fetched):
+                data.wrappedValue = fetched
+            case .failure(let error):
+                print(error)
+            }
+        })
+        isLoading.wrappedValue = false
+        
+        return
+        
+    }
