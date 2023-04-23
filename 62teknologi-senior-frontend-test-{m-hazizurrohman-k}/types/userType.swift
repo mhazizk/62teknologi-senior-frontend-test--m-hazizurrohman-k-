f4df65b54e56aa4f77eb1f7f01fc35da325d4c1b
mhazizk <mhazizk@gmail.com>
2023-04-23 23:38:04 +0700

    Create userType.swift

---
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/types/userType.swift
@@ -0,0 +1,16 @@
+//
+//  userType.swift
+//  62teknologi-senior-frontend-test-{m-hazizurrohman-k}
+//
+//  Created by Haziz on 23/04/23.
+//
+
+import Foundation
+
+
+struct UserType: Codable {
+    let id: String
+    let profile_url: String?
+    let image_url: String?
+    let name: String
+}
