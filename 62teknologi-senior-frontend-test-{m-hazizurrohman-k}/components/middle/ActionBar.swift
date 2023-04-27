b0b619adc0656ad899ee34e109a9578d75623c61
mhazizk <mhazizk@gmail.com>
2023-04-27 22:59:01 +0700

    Update ActionBar.swift

--- 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/components/middle/ActionBar.swift
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/components/middle/ActionBar.swift
@@ -8,35 +8,31 @@
 import Foundation
 import SwiftUI
 
+/**
+ main action button to show on `BusinessDetailsScreen`
+ */
 func ActionBar(business:BusinessDetailsType) -> some View {
     let phone = business.phone
     let website = business.url
     let map = business.location?.address1
-//    let menu = business
     
     
     func openWeb() -> Void {
         guard let url = URL(string: business.url!) else {return}
         return UIApplication.shared.open(url)
     }
-
-    func openMap() -> Void {
-        return
-    }
     
     return HStack(spacing: 16) {
         Spacer()
-        IconButton(iconName: .phone, onPress: { phone in call(number: phone!)})
-        IconButton(iconName: .map, onPress:  openMap)
+        IconButton(iconName: .phone, onPress: {  callPhoneNumber(number: phone!)})
+        IconButton(iconName: .map, onPress:  {openMap(location:business.location!.displayAddress)})
         IconButton(iconName: .globe, onPress: openWeb)
-        IconButton(iconName: .menucard, onPress: openMap)
         Spacer()
     }
     .padding(16)
 }
 
 
-func call(number:String) -> Void {
-    guard let url = URL(string: "tel:\(number)") else { return }
-    return UIApplication.shared.open(url)
-}
+
+
+
