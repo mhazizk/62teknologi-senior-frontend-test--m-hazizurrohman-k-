b7c81ab75abe30a405446d59e2c0c23714389e9f
mhazizk <mhazizk@gmail.com>
2023-04-28 11:13:38 +0700

    Update FilterButton.swift

--- 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/components/high/FilterButton.swift
+++ 62teknologi-senior-frontend-test-{m-hazizurrohman-k}/components/high/FilterButton.swift
@@ -13,20 +13,16 @@ import SwiftUI
  */
 struct FilterButton : View {
     @Binding var params : [String]
-    @State var showMoreFilterSheet = false
-    @State var showSortSheet = false
-    @State var selectedOption = "sort_by=best_match"
     @State var price:[String]? = nil
-    @State var isOpenNow : Bool = true
-    @State var offset : Int = 0
-    @State var location : String = "singapore"
+    @State var isOpenNow : Bool = false
+    @State private var showMoreFilterSheet = false
+    @State private var showSortSheet = false
+    @State private var selectedOption = "sort_by=best_match"
     @State private var sheetContentHeight = CGFloat(0)
     var applyFilter :() -> Void
     
     var body : some View {
-//        ScrollView(.horizontal) {
             HStack(spacing: 8) {
-//                Spacer()
                 
                 switch isParamsChanged(params: params) {
                 case true:
@@ -61,17 +57,9 @@ struct FilterButton : View {
                         Text("Filter")
                             .bold()
                         Spacer()
-//                        Button(action: {
-//                            showMoreFilterSheet = false
-//                        }){
-//                            Image(systemName: "xmark.circle")
-//                        }
-//                        .frame(width: 24, height: 24)
-//                        .aspectRatio(contentMode: .fit)
                     }
                     .padding(16)
                     
-//                    ScrollView {
                         VStack(spacing: 20) {
                             HStack {
                                 Text("Price")
@@ -88,14 +76,13 @@ struct FilterButton : View {
                                                 params = addStringToParams(params: params, stringToAdd: [value])
                                             }
                                             
-                                            print(params)
                                         }) {
                                             Text(key)
-                                                .foregroundColor(isEnabled ? .white : .blue)
+                                                .foregroundColor(isEnabled ? .white : .red)
                                                 .cornerRadius(10)
                                         }
                                         .buttonStyle(.bordered)
-                                        .background(isEnabled ? .blue : .clear )
+                                        .background(isEnabled ? .red : .clear )
                                         .cornerRadius(8)
                                     }
                                 }
@@ -103,41 +90,45 @@ struct FilterButton : View {
                             FilterPicker(title: "Sort by", options: searchQuerySortBy, selected: $selectedOption)
                                 .onChange(of: selectedOption) {
                                     newValue in
-                                    let removedParams = removeStringFromParams(params: params, stringToRemove: ["sort_by"])
+                                    let removedParams = removeStringFromParams(params: params, stringToRemove: ["sort_by="])
                                     params = addStringToParams(params: removedParams, stringToAdd: [newValue])
-                                    print(params)
                                 }
                             
                             Toggle("Open now", isOn: $isOpenNow)
                                 .onChange(of: isOpenNow) { newValue in
-                                    let removedParams = removeStringFromParams(params: params, stringToRemove: ["open_now=true","open_now=false"])
+                                    let removedParams = removeStringFromParams(params: params, stringToRemove: ["open_now="])
                                     if newValue {
                                         params = addStringToParams(params: removedParams,stringToAdd: ["open_now=true"])
                                     } else {
                                         params = addStringToParams(params: removedParams, stringToAdd: ["open_now=false"])
                                     }
-                                    print(params)
                                 }
+                                .tint(.red)
                                 .padding(.trailing,2)
                         }
                         
-//                    }
                     
                     
                     HStack (spacing: 16) {
                         if isParamsChanged(params: params) {
-                            Button("Clear") {
-                                params = defaultParams
+                                Button("Clear") {
+                                    price = nil
+                                    selectedOption = "sort_by=best_match"
+                                    isOpenNow = false
+                                    params = defaultParams
+                                }
+                                .buttonStyle(.bordered)
+                                .tint(.red)
+                                .scaledToFit()
                             }
-                            .buttonStyle(.bordered)
-                            .scaledToFit()
-                        }
                         Button("Apply Filters") {
                             showMoreFilterSheet = false
                             applyFilter()
                         }
                         .buttonStyle(.borderedProminent)
+                        .tint(.red)
                         .scaledToFit()
+                        
                         
                     }
                     .frame(maxWidth: .infinity, alignment: .bottom)
@@ -157,4 +148,4 @@ struct FilterButton : View {
             
         }
     }
-//}
+
